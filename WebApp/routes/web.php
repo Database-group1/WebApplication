05f1a1bf85f31710fe4a5cd7fc3a024d3d4c6e88
secondmine <49423572+secondmine@users.noreply.github.com>
2019-11-28 23:56:02 +0900

    Update web.php
    ベジプラスの各ページのURLを生成
    見積に関しては、一つのページに複数のテーブルのデータを使用する方法の解明ができていないため、暫定的に白石くんのviewのみルーティング

--- WebApp/routes/web.php
+++ WebApp/routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('vegplus', 'VegplusController@index');
+Route::get('vegplus/vegquest', 'VegplusController@question');
+
+Route::get('vegplus/vegquest/watermelon', 'VegplusController@watermelon');
+Route::get('vegplus/vegquest/pumpkin', 'VegplusController@pumpkin');
+Route::get('vegplus/vegquest/radish', 'VegplusController@radish');
+Route::get('vegplus/vegquest/eggplant', 'VegplusController@eggplant');
+Route::get('vegplus/vegquest/pimento', 'VegplusController@pimento');
+Route::get('vegplus/vegquest/cauliflower', 'VegplusController@cauliflower');
+Route::get('vegplus/vegquest/tomato', 'VegplusController@tomato');
+Route::get('vegplus/vegquest/kidney', 'VegplusController@kidney');
+Route::get('vegplus/vegquest/mandragora', 'VegplusController@mandragora');
+Route::get('vegplus/vegquest/soybean', 'VegplusController@soybean');
+Route::get('vegplus/vegquest/cucumber', 'VegplusController@cucumber');
+Route::get('vegplus/vegquest/cabbage', 'VegplusController@cabbage');
+
+/*-----------------------------------------------------------------*/
+
+Route::get('/advice/mitsumori', function(){
+    return view('mitsumori');
+});;
