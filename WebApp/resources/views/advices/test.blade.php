6073a4af1b68237f4d7ebf97b21ae292fa83df6a
Geant4 User <g4user@localhost.localdomain>
2019-11-29 17:15:53 +0900

    adviceViews and advice.css

---
+++ WebApp/resources/views/advices/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head><title>javascript TEST</title></head>
+
+<body>
+
+<script>
+function test_vanilla() {
+    alert("Hello Vanilla Javascript!!");
+}
+</script>
+
+</body>
+</html>
