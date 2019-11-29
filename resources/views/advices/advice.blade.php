a2ea3cda1d0975cd22b6b182127aced79235603d
Geant4 User <g4user@localhost.localdomain>
2019-11-29 21:57:29 +0900

    new

---
+++ resources/views/advices/advice.blade.php
@@ -0,0 +1,16 @@
+<h1>農業チュートリアル</h1>
+<p>これは野菜の育て方アドバイスの野菜選択ページです。</p>
+<a href="{{url('/advice/tomato')}}">トマト</a>、
+<a href="{{url('/advice/piman')}}">ピーマン</a>、
+<a href="{{url('/advice/nasu')}}">ナス</a>、
+<a href="{{url('/advice/kabocha')}}">カボチャ</a>、
+<a href="{{url('/advice/kyuri')}}">キュウリ</a>、
+<a href="{{url('/advice/suika')}}">スイカ</a>、
+<p><a href="{{url('/advice/daikon')}}">大根</a>、
+<a href="{{url('/advice/kyabetsu')}}">キャベツ</a>、
+<a href="{{url('/advice/karifurawa')}}">カリフラワー</a>、
+<a href="{{url('/advice/edamame')}}">枝豆</a>、
+<a href="{{url('/advice/ingenmame')}}">インゲン豆</a>、
+<a href="{{url('/advice/mitsumori')}}">見積り</a>、
+<a href="{{url('/advice/test')}}">test</a>
+<p><a href="{{url('/')}}">トップページ</a></p>
