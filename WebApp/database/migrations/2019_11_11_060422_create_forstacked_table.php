9c993fe48371c42d9bb9c36252d4c5be4f83a2f4
secondmine <m15136@g.metro-cit.ac.jp>
2019-11-29 11:08:42 +0900

    my Vegplus works

---
+++ WebApp/database/migrations/2019_11_11_060422_create_forstacked_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForstackedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forstackeds', function (Blueprint $table) {
+            $table->integer('prefCode')->nullable();
+            $table->string('prefName')->nullable();
+            $table->integer('cityCode')->nullable();
+            $table->string('cityName')->nullable();
+            $table->integer('oldCityCode')->nullable();
+            $table->string('oldCityName')->nullable();
+            $table->integer('dispType');
+            $table->integer('agriculturalLandType');
+            $table->integer('matter');
+            $table->string('label');
+            $table->integer('value_2005');
+            $table->integer('value_2010');
+            $table->integer('value_2015');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forstackeds');
+    }
+}
